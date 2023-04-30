360163441831e83fef3408b75ed075bb56245fd8
Mishael <enyimishael22@gmail.com>
2023-04-30 16:23:29 +0100

    js updates
    Accessed the scores from the data.json file and added it dynamically to
    the page using javascript as well as calculated the total score and
    limited it to a whole number.
    
    The total is the sum of all the individual scores divided by 4.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let rs = document.getElementById('reaction-score')/*rs = reaction score */
-let ms = document.getElementById('memory-score');/* ms = memory-score */
-let vs =  document.getElementById('verbal-score');/* vs = vrbal-score */
-let vis = document.getElementById('visual-score');/* vis = visual-score */
+import scores from "./data.json" assert{type: "json"}
+
+let score0 = scores[0].score;
+let score1 = scores[1].score;
+let score2 = scores[2].score;
+let score3 = scores[3].score;
+
+let rs = document.getElementById('reaction-score').textContent = score0;/*rs = reaction score */
+let ms = document.getElementById('memory-score').textContent = score1;/* ms = memory-score */
+let vs =  document.getElementById('verbal-score').textContent = score2;/* vs = vrbal-score */
+let vis = document.getElementById('visual-score').textContent = score3;/* vis = visual-score */
 
 let total = document.getElementById('total');
+total.textContent = ((rs + ms + vs + vis)/4).toFixed(0)
+console.log(total)
+// console.log(rs, ms, vs, vis)
