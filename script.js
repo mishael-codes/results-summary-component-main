7914df5c220a2b5e01b04193b1f70bc493ddc07d
Mishael <enyimishael22@gmail.com>
2023-04-30 15:23:08 +0100

    js update
    Added a javascript file so that the score and total number can be
    dynamically gotten. For now, I have only accesed them. Also, I added the
    needed id attributes to the necesary elements.

---
+++ script.js
@@ -0,0 +1,6 @@
+let rs = document.getElementById('reaction-score')/*rs = reaction score */
+let ms = document.getElementById('memory-score');/* ms = memory-score */
+let vs =  document.getElementById('verbal-score');/* vs = vrbal-score */
+let vis = document.getElementById('visual-score');/* vis = visual-score */
+
+let total = document.getElementById('total');
